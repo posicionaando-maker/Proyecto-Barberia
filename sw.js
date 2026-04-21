467fd011219aabfa56762a02a0111cf9a698335d
posicionaando-maker <posiciona.ando@gmail.com>
2026-04-21 16:33:43 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = "barbercontrol-v1";
+const urlsToCache = ["/index.html", "/style.css", "/app.js"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
